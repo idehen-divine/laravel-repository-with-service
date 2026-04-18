9b6826d95969ab6dcb3c28a319389da23cb07109
dev L0N3LY <idehendivine16@gmail.com>
2026-04-18 09:16:31 +0100

    Update src/Contracts/Repository.php

--- src/Contracts/Repository.php
+++ src/Contracts/Repository.php
@@ -43,7 +43,7 @@ interface Repository
      *
      * @param  int|mixed  $id
      * @param  array|mixed  $data
-     * @return Model|null
+     * @return Model
      */
     public function update($id, array $data);
 
